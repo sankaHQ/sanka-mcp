89893ac77af11b937ba587de8fbebd5b05e3293a
Haegwan Kim <haegwankim@gmail.com>
2026-06-05 07:57:49 +0900

    Add workspace message MCP tools

--- src/resources/public/public.ts
+++ src/resources/public/public.ts
@@ -21,6 +21,21 @@ import {
   AccountMessageThreadRetrieveParams,
   Threads,
 } from './account-messages/account-messages';
+import * as WorkspaceMessagesAPI from './workspace-messages/workspace-messages';
+import {
+  WorkspaceMessageChannel,
+  WorkspaceMessageListParams,
+  WorkspaceMessages,
+  WorkspaceMessagesData,
+  WorkspaceMessagesResponse,
+  WorkspaceMessageSyncParams,
+  WorkspaceMessageThread,
+  WorkspaceMessageThreadDetail,
+  WorkspaceMessageThreadDetailResponse,
+  WorkspaceMessageThreadMessage,
+  WorkspaceMessageThreadRetrieveParams,
+  Threads as WorkspaceMessageThreads,
+} from './workspace-messages/workspace-messages';
 import * as AssociationsAPI from './associations';
 import {
   Association,
@@ -401,6 +416,9 @@ import {
 
 export class Public extends APIResource {
   accountMessages: AccountMessagesAPI.AccountMessages = new AccountMessagesAPI.AccountMessages(this._client);
+  workspaceMessages: WorkspaceMessagesAPI.WorkspaceMessages = new WorkspaceMessagesAPI.WorkspaceMessages(
+    this._client,
+  );
   associations: AssociationsAPI.Associations = new AssociationsAPI.Associations(this._client);
   imports: ImportsAPI.Imports = new ImportsAPI.Imports(this._client);
   exports: ExportsAPI.Exports = new ExportsAPI.Exports(this._client);
@@ -435,6 +453,7 @@ export class Public extends APIResource {
 }
 
 Public.AccountMessages = AccountMessages;
+Public.WorkspaceMessages = WorkspaceMessages;
 Public.Associations = Associations;
 Public.Imports = Imports;
 Public.Exports = Exports;
@@ -487,6 +506,21 @@ export declare namespace Public {
     type AccountMessageThreadArchiveParams as AccountMessageThreadArchiveParams,
     type AccountMessageThreadReplyParams as AccountMessageThreadReplyParams,
     type Threads as Threads,
+  };
+
+  export {
+    WorkspaceMessages as WorkspaceMessages,
+    type WorkspaceMessageChannel as WorkspaceMessageChannel,
+    type WorkspaceMessageThread as WorkspaceMessageThread,
+    type WorkspaceMessagesData as WorkspaceMessagesData,
+    type WorkspaceMessagesResponse as WorkspaceMessagesResponse,
+    type WorkspaceMessageListParams as WorkspaceMessageListParams,
+    type WorkspaceMessageSyncParams as WorkspaceMessageSyncParams,
+    type WorkspaceMessageThreadMessage as WorkspaceMessageThreadMessage,
+    type WorkspaceMessageThreadDetail as WorkspaceMessageThreadDetail,
+    type WorkspaceMessageThreadDetailResponse as WorkspaceMessageThreadDetailResponse,
+    type WorkspaceMessageThreadRetrieveParams as WorkspaceMessageThreadRetrieveParams,
+    type WorkspaceMessageThreads as WorkspaceMessageThreads,
   };
 
   export {
